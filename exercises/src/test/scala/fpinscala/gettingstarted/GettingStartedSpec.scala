1ac29436217defa702abe83069c64e0ac6be8531
doi_kenji <doi_kenji@cyberagent.co.jp>
2014-08-01 17:59:52 +0900

    Chaptest2 Done

--- exercises/src/test/scala/fpinscala/gettingstarted/GettingStartedSpec.scala
+++ exercises/src/test/scala/fpinscala/gettingstarted/GettingStartedSpec.scala
@@ -1,3 +1,5 @@
+package fpinscala.gettingstarted
+
 import org.specs2.mutable._
 
 class GettingStartedSpec extends Specification {
@@ -11,6 +13,22 @@ class GettingStartedSpec extends Specification {
     }
     "end with 'world'" in {
       "Hello world" must endWith("world")
+    }
+  }
+  "fib" should {
+    "return 3 when 5 given" in {
+      MyModule.fib(5) must_== 3
+    }
+    "return 0 when 1 given" in {
+      MyModule.fib(1) must_== 0
+    }
+  }
+  "isSorted" should {
+    "return true" in {
+      PolymorphicFunctions.isSorted(Array(1, 2), ((left: Int, right: Int) => left < right)) must_== true
+    }
+    "return false" in {
+      PolymorphicFunctions.isSorted(Array(1, 2), ((left: Int, right: Int) => left > right)) must_== false
     }
   }
 }
